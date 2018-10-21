ddf9a0ecdf8b89e04d8ed485a4b1946c4bb1fbb3
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-21 12:59:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,11 +10,13 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  
 
 end
 
 def words_five_letters_long(text)
-
+  array = text.to_a
+  array.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
