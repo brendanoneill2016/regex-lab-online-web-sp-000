732eff1bb85cf97a164f8ad1af978a12c116a8f2
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-21 14:13:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+  array = text.split
+  array.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
